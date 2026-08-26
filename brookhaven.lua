71e8c979959fe3d76f1aa365f884f087b75afd22
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 17:25:48 -0300

    Update Apenas_eu362

--- brookhaven.lua
+++ brookhaven.lua
@@ -3,5 +3,6 @@ return {
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
     "azakel_ofc", -- Ruan 26/08/26 22/05/29
-    "hdjdhdhurhrjtl" -- TUBERS93 AP 26/08/26 29/08/26
+    "hdjdhdhurhrjtl", -- TUBERS93 AP 26/08/26 29/08/26
+    "Apenas_eu362" -- الوحدة • 26/08/26 26/07/28
 }
