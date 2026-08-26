0f7824981abe47849eb5015843cd0544c71a5295
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 14:25:11 -0300

    Update hdjdhdhurhrjtl

--- brookhaven.lua
+++ brookhaven.lua
@@ -2,5 +2,6 @@ return {
     "Breno010L", -- Bazuka
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
-    "azakel_ofc" -- Ruan 26/08/26 22/05/29
+    "azakel_ofc", -- Ruan 26/08/26 22/05/29
+    "hdjdhdhurhrjtl" -- TUBERS93 AP 26/08/26 29/08/26
 }
