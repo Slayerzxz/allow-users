4fc89e00142101fd1c5095aae5c91d0f73c7a0c9
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 12:20:28 -0300

    Remove xinglin037

--- brookhaven.lua
+++ brookhaven.lua
@@ -2,6 +2,5 @@ return {
     "Breno010L", -- Bazuka
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
-    "itz_kadu22", -- Bazuka հվ 26/08/26 11/01/54
-    "xinglin037" -- Gabriel 26/08/26 26/09/26
+    "itz_kadu22" -- Bazuka հվ 26/08/26 11/01/54
 }
