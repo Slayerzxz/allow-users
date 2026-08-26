bfdfdefc106aa25229bc0e5d7b057d8584d44443
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 12:17:23 -0300

    Update xinglin037

--- brookhaven.lua
+++ brookhaven.lua
@@ -2,5 +2,6 @@ return {
     "Breno010L", -- Bazuka
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
-    "itz_kadu22" -- Bazuka հվ 26/08/26 11/01/54
+    "itz_kadu22", -- Bazuka հվ 26/08/26 11/01/54
+    "xinglin037" -- Gabriel 26/08/26 26/09/26
 }
