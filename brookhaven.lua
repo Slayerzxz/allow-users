c0cbd70884d70efe71a3c58bc57afd3e81a905b8
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 01:33:01 -0300

    Remove teste

--- brookhaven.lua
+++ brookhaven.lua
@@ -2,6 +2,5 @@ return {
     "Breno010L", -- Bazuka
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
-    "teste", -- Bazuka հվ 26/08/26 27/08/26
     "tst" -- Slayer Hub | Admin 26/08/26 27/08/26
 }
