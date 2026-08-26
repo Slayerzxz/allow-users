4e9f9dff70b4e7bc5a1b9ace98572ac82ae0c311
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 20:27:33 -0300

    Update becars_3

--- brookhaven.lua
+++ brookhaven.lua
@@ -4,5 +4,6 @@ return {
     "kzrrzs4r", -- Beca
     "azakel_ofc", -- Ruan 26/08/26 22/05/29
     "hdjdhdhurhrjtl", -- TUBERS93 AP 26/08/26 29/08/26
-    "Apenas_eu362" -- الوحدة • 26/08/26 26/07/28
+    "Apenas_eu362", -- الوحدة • 26/08/26 26/07/28
+    "becars_3" -- suzuya 26/08/26 26/08/90
 }
