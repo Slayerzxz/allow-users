78eee3bff42b6e69385864250a7eb9edcf4e756a
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-26 21:27:25 -0300

    Auto remove expirados: becars_3

--- brookhaven.lua
+++ brookhaven.lua
@@ -4,6 +4,5 @@ return {
     "kzrrzs4r", -- Beca
     "azakel_ofc", -- Ruan 26/08/26 22/05/29
     "hdjdhdhurhrjtl", -- TUBERS93 AP 26/08/26 29/08/26
-    "Apenas_eu362", -- الوحدة • 26/08/26 26/07/28
-    "becars_3" -- suzuya 26/08/26 26/08/90
+    "Apenas_eu362" -- الوحدة • 26/08/26 26/07/28
 }
