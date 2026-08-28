481a79e0195dfdf9d4f2792cd20a91c918f251da
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-27 22:38:57 -0300

    Update vitinhofofinho54

--- brookhaven.lua
+++ brookhaven.lua
@@ -6,5 +6,6 @@ return {
     "hdjdhdhurhrjtl", -- TUBERS93 AP 26/08/26 29/08/26
     "Apenas_eu362", -- الوحدة • 26/08/26 26/07/28
     "INC0MUUUU90", -- suzuya 27/08/26 25/11/26
-    "fernandomt5" -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 27/08/26 12/01/54
+    "fernandomt5", -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 27/08/26 12/01/54
+    "vitinhofofinho54" -- Tugsahuuauau 27/08/26 27/12/26
 }
