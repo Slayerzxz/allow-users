40572d8bd984f246c0dd7ea796a0b991d1222354
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-29 01:27:25 -0300

    Auto remove expirados: hdjdhdhurhrjtl

--- brookhaven.lua
+++ brookhaven.lua
@@ -3,7 +3,6 @@ return {
     "StaffordMakayla783", -- bazuka
     "kzrrzs4r", -- Beca
     "azakel_ofc", -- Ruan 26/08/26 22/05/29
-    "hdjdhdhurhrjtl", -- TUBERS93 AP 26/08/26 29/08/26
     "Apenas_eu362", -- الوحدة • 26/08/26 26/07/28
     "INC0MUUUU90", -- suzuya 27/08/26 25/11/26
     "fernandomt5", -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 27/08/26 12/01/54
