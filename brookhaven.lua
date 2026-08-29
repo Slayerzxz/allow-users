8b5b91206d640b3e437e27ce76ba6a6a3fcf49b2
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-29 02:11:40 -0300

    Update itz_blackzinh01

--- brookhaven.lua
+++ brookhaven.lua
@@ -6,5 +6,6 @@ return {
     "Apenas_eu362", -- الوحدة • 26/08/26 26/07/28
     "INC0MUUUU90", -- suzuya 27/08/26 25/11/26
     "fernandomt5", -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 27/08/26 12/01/54
-    "vitinhofofinho54" -- Tugsahuuauau 27/08/26 27/12/26
+    "vitinhofofinho54", -- Tugsahuuauau 27/08/26 27/12/26
+    "itz_blackzinh01" -- max 29/08/26 13/01/54
 }
