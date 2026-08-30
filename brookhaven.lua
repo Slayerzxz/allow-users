ac1572007c710e20190389a02b6138534e468a7f
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-08-30 11:31:06 -0300

    Update darkizinha_6767

--- brookhaven.lua
+++ brookhaven.lua
@@ -12,5 +12,6 @@ return {
     "joaoking667", -- joaoexe66 29/08/26 29/09/26
     "XmoroAx", -- idkwhat_putinmyname_pop 29/08/26 01/09/26
     "Hiimnew928289alt2", -- 👑👑 30/08/26 04/09/26
-    "Maria91062993" -- Slayer Hub | Admin 30/08/26 09/09/26
+    "Maria91062993", -- Slayer Hub | Admin 30/08/26 09/09/26
+    "darkizinha_6767" -- Slayer Hub | Moderation 30/08/26 29/09/26
 }
