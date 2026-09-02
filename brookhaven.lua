90c9c3ab16cba117ee15cb977938009efa5b83d2
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-02 01:30:28 -0300

    Remove Realfe32

--- brookhaven.lua
+++ brookhaven.lua
@@ -25,6 +25,5 @@ return {
     "nicolas007zicapro", -- Nicolas 02/09/26 09/09/26
     "eo_vtz2", -- victor 02/09/26 09/09/26
     "Ramon_6390", -- SOARES 02/09/26 09/09/26
-    "Realfe32", -- Pedro_021 02/09/26 09/09/26
     "Realfe23" -- Pedro_021 02/09/26 25/05/27
 }
