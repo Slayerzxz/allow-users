170c8b0087404b963ae800c8169160dc23a50461
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-02 19:32:12 -0300

    Update lorenzOo_1589

--- brookhaven.lua
+++ brookhaven.lua
@@ -26,5 +26,6 @@ return {
     "eo_vtz2", -- victor 02/09/26 09/09/26
     "Ramon_6390", -- SOARES 02/09/26 09/09/26
     "Realfe23", -- Pedro_021 02/09/26 02/10/26
-    "Madaranoobb80" -- Beatriz 02/09/26 05/09/26
+    "Madaranoobb80", -- Beatriz 02/09/26 05/09/26
+    "lorenzOo_1589" -- Gugugaga 02/09/26 02/10/26
 }
