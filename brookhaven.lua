1ca293012e816d40f45050ab7c52a5f8d51ba6a0
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 14:53:00 -0300

    Admin panel: Johnrairo

--- brookhaven.lua
+++ brookhaven.lua
@@ -70,5 +70,6 @@ return {
     "jacintoMatador058", -- cabritozin_08486 03/09/26 06/09/26
     "lengemdary4", -- braiz804 03/09/26 06/09/26
     "santi2223bbknvhc", -- santiago009284 03/09/26 06/09/26
-    "kallebplayof" -- skyleess0 03/09/26 06/09/26
+    "kallebplayof", -- skyleess0 03/09/26 06/09/26
+    "Johnrairo" -- dhx.00_17917 03/09/26 06/09/26
 }
