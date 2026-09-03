2ae0b78cc374ae636f60af566f72c0bc604169fb
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 18:19:02 -0300

    Admin panel: leoXD12007

--- brookhaven.lua
+++ brookhaven.lua
@@ -85,5 +85,6 @@ return {
     "miguel_100733", -- vitor170549 03/09/26 06/09/26
     "dark155135", -- izumi._.4 03/09/26 06/09/26
     "dudusourei8", -- lucifer7666sj 03/09/26 06/09/26
-    "lucianodum0" -- pintodavi 03/09/26 06/09/26
+    "lucianodum0", -- pintodavi 03/09/26 06/09/26
+    "leoXD12007" -- mati03492 03/09/26 06/09/26
 }
