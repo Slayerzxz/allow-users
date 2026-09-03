b486c5edcdf6e807fa963a1de3190f564c5921ae
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 14:02:33 -0300

    Admin panel: Omar324w

--- brookhaven.lua
+++ brookhaven.lua
@@ -49,5 +49,6 @@ return {
     "lindo128958", -- luiz032492 03/09/26 06/09/26
     "HorroresGame", -- drezzyzadas 03/09/26 06/09/26
     "XmoroAx", -- idkwhat_putinmyname_pop 03/09/26 06/09/26
-    "6hey_yeh90" -- alessandro_1234567890 03/09/26 06/09/26
+    "6hey_yeh90", -- alessandro_1234567890 03/09/26 06/09/26
+    "Omar324w" -- fireflame324 03/09/26 06/09/26
 }
