fb9b24bee7001e201c2100f7c15dbda1cb6d760b
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 14:17:20 -0300

    Admin panel: Baconhello990

--- brookhaven.lua
+++ brookhaven.lua
@@ -54,5 +54,6 @@ return {
     "hahahah", -- noman04550 03/09/26 06/09/26
     "standinawenc", -- vito017233 03/09/26 06/09/26
     "Horuxo12", -- ztrx0646 03/09/26 06/09/26
-    "Hatimle82" -- hatim352. 03/09/26 06/09/26
+    "Hatimle82", -- hatim352. 03/09/26 06/09/26
+    "Baconhello990" -- 9_artem_9 03/09/26 06/09/26
 }
