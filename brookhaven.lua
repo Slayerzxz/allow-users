dcd8783d07e717ac2426f7b321f326ace677d337
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 18:30:42 -0300

    Update Surf_2261

--- brookhaven.lua
+++ brookhaven.lua
@@ -89,5 +89,6 @@ return {
     "leoXD12007", -- mati03492 03/09/26 06/09/26
     "matheus45534", -- ys_edx. 03/09/26 06/09/26
     "Sub_Fk7xisZ", -- fiuk7xz.solo_94663 03/09/26 06/09/26
-    "Otavio3D10" -- mysterioso._71615 03/09/26 06/09/26
+    "Otavio3D10", -- mysterioso._71615 03/09/26 06/09/26
+    "Surf_2261" -- Wagneriz 03/09/26 12/12/26
 }
