e574aef77d1161726e5a521869d0e6c4bbadcf12
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 23:38:42 -0300

    Admin panel: tomzinhojj

--- brookhaven.lua
+++ brookhaven.lua
@@ -98,5 +98,6 @@ return {
     "V4lkblen736", -- leonardo990544 03/09/26 06/09/26
     "yakov812307", -- yantitas36 03/09/26 06/09/26
     "teajudonono99", -- daviziin0581 03/09/26 06/09/26
-    "dan_luiz666" -- jefin_da_boca 03/09/26 06/09/26
+    "dan_luiz666", -- jefin_da_boca 03/09/26 06/09/26
+    "tomzinhojj" -- draxmdx_51131 03/09/26 06/09/26
 }
