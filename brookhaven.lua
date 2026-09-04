3a43a6b56af85de1fce40f088c1b68b330d968c9
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-04 01:08:42 -0300

    Admin panel: baconlindissmo31

--- brookhaven.lua
+++ brookhaven.lua
@@ -107,5 +107,6 @@ return {
     "Rip_zonedes", -- carlosmebeixa 03/09/26 06/09/26
     "PREFIRO_FREEFIRE047", -- comeveia0713_10673 03/09/26 06/09/26
     "kamily128374646", -- alice06004 04/09/26 07/09/26
-    "davi_pa99h" -- daviflores0302 04/09/26 07/09/26
+    "davi_pa99h", -- daviflores0302 04/09/26 07/09/26
+    "baconlindissmo31" -- yu050171 04/09/26 07/09/26
 }
