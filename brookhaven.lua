8684c2169001f46c5605c1317c442dcc93c8077d
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-03 23:36:55 -0300

    Admin panel: yakov812307

--- brookhaven.lua
+++ brookhaven.lua
@@ -95,5 +95,6 @@ return {
     "666x6663o", -- Tnt_Stree_life 03/09/26 06/09/26
     "jhonnysiuza9", -- jhonny244_. 03/09/26 06/09/26
     "kauaenay", -- kaue082039 03/09/26 06/09/26
-    "V4lkblen736" -- leonardo990544 03/09/26 06/09/26
+    "V4lkblen736", -- leonardo990544 03/09/26 06/09/26
+    "yakov812307" -- yantitas36 03/09/26 06/09/26
 }
