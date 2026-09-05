900f9f4161c702505cb229290ec4d56e10e08427
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-05 15:29:27 -0300

    Admin panel: jahmeil12334

--- brookhaven.lua
+++ brookhaven.lua
@@ -116,5 +116,6 @@ return {
     "Kaio_220616", -- arthurzin098 05/09/26 08/09/26
     "blackn444hey", -- Coringa 05/09/26 05/10/26
     "roblox038pk", -- Little Haze 05/09/26 08/09/26
-    "Lizwxy9" -- lipezinho_chuh 05/09/26 08/09/26
+    "Lizwxy9", -- lipezinho_chuh 05/09/26 08/09/26
+    "jahmeil12334" -- shz_calvo_71650 05/09/26 08/09/26
 }
