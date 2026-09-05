cbd0535b9d277201421037a476a320c62d470fcb
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-05 20:34:26 -0300

    Admin panel: FFH4XV79J

--- brookhaven.lua
+++ brookhaven.lua
@@ -133,5 +133,6 @@ return {
     "Hacks1Hav3", -- busy_cheetah_84495 05/09/26 08/09/26
     "gabrieujjjii", -- jusapex. 05/09/26 08/09/26
     "jester9729", -- Jvzin 05/09/26 20/01/54
-    "9nn9_anniesister" -- sys928_ 05/09/26 08/09/26
+    "9nn9_anniesister", -- sys928_ 05/09/26 08/09/26
+    "FFH4XV79J" -- joaogabriel083198 05/09/26 08/09/26
 }
