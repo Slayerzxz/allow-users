bb540343e65168d89be20d243fdf50c301194033
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-05 23:51:35 -0300

    Update kawasaki2812

--- brookhaven.lua
+++ brookhaven.lua
@@ -141,5 +141,6 @@ return {
     "Adrian_gameryt0", -- adrianxzn_ 05/09/26 08/09/26
     "umboyqualquer0011", -- rafael014508 05/09/26 08/09/26
     "felicidade955", -- carlos67noob_81784 05/09/26 08/09/26
-    "GUI_FDF123" -- guilherme096984 05/09/26 08/09/26
+    "GUI_FDF123", -- guilherme096984 05/09/26 08/09/26
+    "kawasaki2812" -- Kaio 05/09/26 21/01/54
 }
