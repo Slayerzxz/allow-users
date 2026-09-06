90a0ad549198ac321a6860bdce0b709b7d7752cb
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-06 14:43:10 -0300

    Admin panel: alusonve

--- brookhaven.lua
+++ brookhaven.lua
@@ -73,5 +73,6 @@ return {
     "L7PEDRO_GELADO", -- XxXxXxX 06/09/26 07/09/26
     "Auraz_182", -- 𝔄𝔘ℜ𝔄ℨ_ 06/09/26 07/09/26
     "kawazaki2812", -- Kaio 06/09/26 22/01/54
-    "sombr229" -- spinossaurs 06/09/26 09/09/26
+    "sombr229", -- spinossaurs 06/09/26 09/09/26
+    "alusonve" -- alias087909 06/09/26 09/09/26
 }
