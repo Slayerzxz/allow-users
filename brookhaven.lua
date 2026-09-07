6ecd7099491f68a9f08cb7068853d8afb63279ad
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-07 17:52:11 -0300

    Admin panel: osakuzao011

--- brookhaven.lua
+++ brookhaven.lua
@@ -95,5 +95,6 @@ return {
     "LucasGamer_TDR9", -- velky_testes 07/09/26 10/09/26
     "Trialuser1235", -- alemao_hittler 07/09/26 10/09/26
     "ltz_diegosamtos", -- diego051972 07/09/26 10/09/26
-    "rjl177" -- injustica_do_roblox 07/09/26 10/09/26
+    "rjl177", -- injustica_do_roblox 07/09/26 10/09/26
+    "osakuzao011" -- lucass288888 07/09/26 10/09/26
 }
