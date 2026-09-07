6def6f56576f8d24351ea2e80b1bb95eb877c65d
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-07 19:36:02 -0300

    Admin panel: jeieheiur

--- brookhaven.lua
+++ brookhaven.lua
@@ -97,5 +97,6 @@ return {
     "ltz_diegosamtos", -- diego051972 07/09/26 10/09/26
     "rjl177", -- injustica_do_roblox 07/09/26 10/09/26
     "osakuzao011", -- lucass288888 07/09/26 10/09/26
-    "real_slime432" -- sebastianzeta_ 07/09/26 10/09/26
+    "real_slime432", -- sebastianzeta_ 07/09/26 10/09/26
+    "jeieheiur" -- joaopalo123 07/09/26 10/09/26
 }
