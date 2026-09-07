4cd851ca59c3d5a2e7820c62853403561c21f889
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-07 17:39:52 -0300

    Admin panel: ltz_diegosamtos

--- brookhaven.lua
+++ brookhaven.lua
@@ -93,5 +93,6 @@ return {
     "dino_game134", -- ᴴ⁷ᶻ 07/09/26 07/10/26
     "M2FA4B5", -- marcostipaulo 07/09/26 10/09/26
     "LucasGamer_TDR9", -- velky_testes 07/09/26 10/09/26
-    "Trialuser1235" -- alemao_hittler 07/09/26 10/09/26
+    "Trialuser1235", -- alemao_hittler 07/09/26 10/09/26
+    "ltz_diegosamtos" -- diego051972 07/09/26 10/09/26
 }
