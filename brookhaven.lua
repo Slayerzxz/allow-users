0ce7654efdbdabd43c96b8f7e112941f00c11eae
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-07 15:50:48 -0300

    Admin panel: LucasGamer_TDR9

--- brookhaven.lua
+++ brookhaven.lua
@@ -91,5 +91,6 @@ return {
     "Xinglin037", -- Davi 07/09/26 14/09/26
     "Samuel_fortao", -- Sma 07/09/26 14/09/26
     "dino_game134", -- ᴴ⁷ᶻ 07/09/26 07/10/26
-    "M2FA4B5" -- marcostipaulo 07/09/26 10/09/26
+    "M2FA4B5", -- marcostipaulo 07/09/26 10/09/26
+    "LucasGamer_TDR9" -- velky_testes 07/09/26 10/09/26
 }
