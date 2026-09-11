1bd646442c42e12ea213d4146176d3639f8ce363
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-10 23:27:07 -0300

    Update onlymothers3

--- brookhaven.lua
+++ brookhaven.lua
@@ -32,7 +32,7 @@ return {
     "roblox_user_8505113953", -- Miguel Mod 07/09/26 07/10/26
     "cria223696", -- alluring_possum_20486 08/09/26 11/09/26
     "TrollLinux", -- Yuki (Owner) 08/09/26 28/09/26
-    "onlymothers3", -- John813 08/09/26 08/10/26
+    "onlymothers3", -- John813 10/09/26 12/10/26
     "marcosw12395", -- marcos18182 08/09/26 11/09/26
     "mercuriobrx67", -- santozz0705_34103 08/09/26 11/09/26
     "bazulaque", -- vtzin07506 08/09/26 11/09/26
