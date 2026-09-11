edf8ddaabb6c9e831420acd47a569329a3527f75
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-11 01:01:14 -0300

    Admin panel: cpa548

--- brookhaven.lua
+++ brookhaven.lua
@@ -38,5 +38,6 @@ return {
     "Breno010L", -- Bazuka հվ 11/09/26 27/01/54
     "matheus_itz00", -- 𝖆𝖑𝖊𝖒ã𝖔🇩🇪 11/09/26 07/06/29
     "Sofia13nicolle", -- JAPONExx 11/09/26 27/01/54
-    "ITACHI_UCHIHA24362" -- ! 𝐉𝐮𝐚𝐧.ｋｋｋ 11/09/26 27/01/54
+    "ITACHI_UCHIHA24362", -- ! 𝐉𝐮𝐚𝐧.ｋｋｋ 11/09/26 27/01/54
+    "cpa548" -- zealous_flamingo_86022 11/09/26 14/09/26
 }
