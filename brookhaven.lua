d0361945a223ed0188a45341e1764f25cd41db7d
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-11 18:42:38 -0300

    Update Sadboy_LIEB

--- brookhaven.lua
+++ brookhaven.lua
@@ -44,5 +44,6 @@ return {
     "Blackn444hey", -- Coringa 11/09/26 11/10/26
     "LHQIAMAS", -- LH.QIA+ 11/09/26 21/09/26
     "D4rkzin_312", -- XxXxXxX 11/09/26 27/01/54
-    "Ligapra_90028922" -- Mkzinho0 11/09/26 12/10/26
+    "Ligapra_90028922", -- Mkzinho0 11/09/26 12/10/26
+    "Sadboy_LIEB" -- GTR R35 11/09/26 07/06/29
 }
