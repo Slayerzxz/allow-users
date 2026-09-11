60fbc322aca6cf304ffb6bbadd5060b67d367d89
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-11 14:25:16 -0300

    Remove Bot4Dev4

--- brookhaven.lua
+++ brookhaven.lua
@@ -42,7 +42,6 @@ return {
     "RIP_KAUA836", -- kaua073195 11/09/26 14/09/26
     "Ne0nFr0stbite7320", -- 𝓝𝓮𝔁𝔁𝓾𝓼 🩸 11/09/26 27/01/54
     "Blackn444hey", -- Coringa 11/09/26 11/10/26
-    "Bot4Dev4", -- Yuki 11/09/26 13/09/26
     "LHQIAMAS", -- LH.QIA+ 11/09/26 21/09/26
     "D4rkzin_312" -- XxXxXxX 11/09/26 27/01/54
 }
