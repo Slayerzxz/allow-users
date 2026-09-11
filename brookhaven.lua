509d40c2806af3556feda45db4b7da20a77adce3
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-11 09:39:11 -0300

    Update Sofia13nicolle

--- brookhaven.lua
+++ brookhaven.lua
@@ -37,7 +37,7 @@ return {
     "Ramon_6390", -- SOARES_G 11/09/26 27/01/54
     "Breno010L", -- Bazuka հվ 11/09/26 27/01/54
     "matheus_itz00", -- 𝖆𝖑𝖊𝖒ã𝖔🇩🇪 11/09/26 07/06/29
-    "Sofia13nicolle", -- JAPONExx 11/09/26 27/01/54
+    "Sofia13nicolle", -- ♛ִֶ🇧🇷་༘JAPONExx 11/09/26 27/01/54
     "ITACHI_UCHIHA24362", -- ! 𝐉𝐮𝐚𝐧.ｋｋｋ 11/09/26 27/01/54
     "cpa548", -- Kj 11/09/26 27/01/54
     "RIP_KAUA836", -- kaua073195 11/09/26 14/09/26
