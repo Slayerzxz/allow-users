eb8dbdec4ee79ac0a9183a47559d06297587eb8b
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-12 20:05:26 -0300

    Update ByteBandit_ofici

--- brookhaven.lua
+++ brookhaven.lua
@@ -59,5 +59,5 @@ return {
     "tomzinhojj", -- Dexter0111 12/09/26 13/09/26
     "cobridjd", -- Zapones de sp 12/09/26 12/10/26
     "Enzo_7m890", -- Gulosão_kkkk 12/09/26 10/01/65
-    "ByteBandit_ofici" -- 𝔸𝕦𝕣𝕒𝕫𝕫𝕫 12/09/26 28/01/54
+    "ByteBandit_ofici" -- ByteBandit 12/09/26 10/01/65
 }
