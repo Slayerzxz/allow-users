25db40549a8167556b640f4c746562f45fcb1456
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-11 23:22:27 -0300

    Remove ligapra_90028922

--- brookhaven.lua
+++ brookhaven.lua
@@ -52,6 +52,5 @@ return {
     "nehdcjdjgd", -- joaolucas07441 11/09/26 14/09/26
     "SOU_532", -- griloobs 11/09/26 14/09/26
     "icarofff379r", -- aloneunk 11/09/26 14/09/26
-    "ligapra_90028922", -- Mkzinho0 11/09/26 11/10/26
     "Kryzen7804" -- Carlinzk7 11/09/26 16/09/26
 }
