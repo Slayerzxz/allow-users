13dad6b9b17fc0c070ecb656c2ec2173a59410c0
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-12 03:10:25 -0300

    Update luminina6

--- brookhaven.lua
+++ brookhaven.lua
@@ -55,5 +55,6 @@ return {
     "ligapra_90028922", -- Mkzinho0 11/09/26 10/11/26
     "BlackN444hey", -- Coringa 12/09/26 11/11/26
     "eufarmoaura009", -- joaozin04449 12/09/26 15/09/26
-    "DJ_GIZIM12358" -- dervXbnz 12/09/26 28/01/54
+    "DJ_GIZIM12358", -- dervXbnz 12/09/26 28/01/54
+    "luminina6" -- Antonio 12/09/26 13/09/26
 }
