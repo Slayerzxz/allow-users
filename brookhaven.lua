7061a83b517e71abd629312c80ddd7bf751bdcf8
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-13 01:09:48 -0300

    Update Breno010L

--- brookhaven.lua
+++ brookhaven.lua
@@ -55,5 +55,6 @@ return {
     "cobridjd", -- Zapones de sp 12/09/26 12/10/26
     "enzo_7m890", -- Gulosão_kkkk 12/09/26 10/01/65
     "ByteBandit_ofici", -- ByteBandit 12/09/26 10/01/65
-    "madaranoobb80" -- スバル 12/09/26 12/10/26
+    "madaranoobb80", -- スバル 12/09/26 12/10/26
+    "Breno010L" -- 7812 13/09/26 14/09/26
 }
