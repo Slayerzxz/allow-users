5f60639f7b317599f5a2561bccff54c9dc33ee78
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-13 11:21:41 -0300

    Remove S2_Maviiis

--- brookhaven.lua
+++ brookhaven.lua
@@ -9,7 +9,6 @@ return {
     "MV_ttk1234567", -- Yuki 29/08/26 25/05/29
     "joaoking667", -- joaoexe66 29/08/26 29/09/26
     "darkizinha_6767", -- Slayer Hub | Moderation 30/08/26 29/09/26
-    "S2_Maviiis", -- Bazuka հվ 30/08/26 26/05/29
     "Keel_m5", -- دونا 31/08/26 01/10/26
     "Riftod37", -- Dazzy 03/09/26 28/09/26
     "Matheus7374568", -- Filha da puta gamers 01/09/26 05/09/27
