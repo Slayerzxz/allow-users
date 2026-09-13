bba7276e9c2e769516372d4c9e59c258b1a1d8d9
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-13 13:12:59 -0300

    Update sofia_fofa72725

--- brookhaven.lua
+++ brookhaven.lua
@@ -54,5 +54,6 @@ return {
     "ByteBandit_ofici", -- ByteBandit 12/09/26 10/01/65
     "madaranoobb80", -- スバル 12/09/26 12/10/26
     "sofia13nicolle", -- ♛ִֶ🇧🇷་༘JAPONExx 13/09/26 29/01/54
-    "Thomas_rico3" -- Credo um mobile 🥷⌨️🖱 13/09/26 16/09/26
+    "Thomas_rico3", -- Credo um mobile 🥷⌨️🖱 13/09/26 16/09/26
+    "sofia_fofa72725" -- 《Herdeiro》Maxzin 13/09/26 16/09/26
 }
