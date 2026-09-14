8176a3b6490a828e75cd8b9eeee7609732d3178d
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-14 01:14:28 -0300

    Auto remove expirados: xinglin037, Samuel_fortao, RIP_KAUA836, oToddyZx, wuwau7w8q8q

--- brookhaven.lua
+++ brookhaven.lua
@@ -19,8 +19,6 @@ return {
     "dan_luiz666", -- Dennys 05/09/26 03/01/27
     "kawazaki2812", -- Kaio 11/09/26 27/01/54
     "dudusourei8", -- Lúcifer 06/09/26 22/01/54
-    "xinglin037", -- Davi 07/09/26 14/09/26
-    "Samuel_fortao", -- Sma 07/09/26 14/09/26
     "dino_game134", -- ᴴ⁷ᶻ 07/09/26 07/10/26
     "roblox_user_8505113953", -- Miguel Mod 07/09/26 07/10/26
     "TrollLinux", -- Yuki (Owner) 08/09/26 28/09/26
@@ -33,18 +31,10 @@ return {
     "matheus_itz00", -- 𝖆𝖑𝖊𝖒ã𝖔🇩🇪 11/09/26 07/06/29
     "ITACHI_UCHIHA24362", -- ! 𝐉𝐮𝐚𝐧.ｋｋｋ 11/09/26 27/01/54
     "cpa548", -- Kj 11/09/26 27/01/54
-    "RIP_KAUA836", -- kaua073195 11/09/26 14/09/26
     "Ne0nFr0stbite7320", -- 𝓝𝓮𝔁𝔁𝓾𝓼 🩸 11/09/26 27/01/54
     "LHQIAMAS", -- LH.QIA+ 11/09/26 21/09/26
     "D4rkzin_312", -- XxXxXxX 11/09/26 27/01/54
     "Sadboy_LIEB", -- GTR R35 11/09/26 03/05/51
-    "oToddyZx", -- contaaltdotnzz 11/09/26 14/09/26
-    "wuwau7w8q8q", -- phzinho1230098 11/09/26 14/09/26
-    "tonygoncs5", -- kevinho_gk_tps 11/09/26 14/09/26
-    "jshdhahdg", -- pedrinxz070743 11/09/26 14/09/26
-    "nehdcjdjgd", -- joaolucas07441 11/09/26 14/09/26
-    "SOU_532", -- griloobs 11/09/26 14/09/26
-    "icarofff379r", -- aloneunk 11/09/26 14/09/26
     "Kryzen7804", -- Carlinzk7 11/09/26 16/09/26
     "ligapra_90028922", -- Mkzinho0 11/09/26 10/11/26
     "BlackN444hey", -- Coringa 12/09/26 11/11/26
