626afdeabd08d3d71d9a930e8afc27ab1eab0f70
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-14 18:16:31 -0300

    Admin panel: Eo_OfoX

--- brookhaven.lua
+++ brookhaven.lua
@@ -63,5 +63,6 @@ return {
     "jvzin9729", -- Jvzin 14/09/26 29/01/54
     "profiadolz1", -- dadas__444 14/09/26 17/09/26
     "vitor_hugo01234", -- vitim_027_ 14/09/26 17/09/26
-    "Rip_icone1" -- kaleby0341 14/09/26 17/09/26
+    "Rip_icone1", -- kaleby0341 14/09/26 17/09/26
+    "Eo_OfoX" -- maria_criarj 14/09/26 17/09/26
 }
