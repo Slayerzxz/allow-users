1f53f7a25d5d22a76dd0bb18b0cdf155609c2dc2
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-14 01:27:59 -0300

    Admin panel: GhostTinyBear

--- brookhaven.lua
+++ brookhaven.lua
@@ -55,5 +55,6 @@ return {
     "TrueblueSeth5", -- thebestimpactace_40654 13/09/26 16/09/26
     "jhonnysiuza9", -- jhonny244_. 13/09/26 16/09/26
     "WEVERTON68935", -- 1350360256 13/09/26 16/09/26
-    "RIP_indra98519" -- datk2244 14/09/26 17/09/26
+    "RIP_indra98519", -- datk2244 14/09/26 17/09/26
+    "GhostTinyBear" -- slk_compenw 14/09/26 17/09/26
 }
