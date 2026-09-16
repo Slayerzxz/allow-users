d2f67e18d716418c99c573f50a73b0829896f752
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 20:22:57 -0300

    Update fernandomt5

--- brookhaven.lua
+++ brookhaven.lua
@@ -72,5 +72,6 @@ return {
     "enzome156", -- iludido_2.0 15/09/26 18/09/26
     "judelcy7", -- SolteirinhoBALA 16/09/26 19/09/26
     "Joan45858", -- Liviablox 16/09/26 31/01/54
-    "Ravis_contasegund" -- joaoexe66 16/09/26 21/09/26
+    "Ravis_contasegund", -- joaoexe66 16/09/26 21/09/26
+    "fernandomt5" -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 16/09/26 31/01/54
 }
