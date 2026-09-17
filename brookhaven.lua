d90bd5e188a47e9279dc1ca3a55eccb8f055116b
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:24:59 -0300

    Admin panel: black_01428

--- brookhaven.lua
+++ brookhaven.lua
@@ -82,5 +82,6 @@ return {
     "sharkzin2686", -- joaolucas0182_16889 16/09/26 19/09/26
     "Piloto0_07", -- w3l1n6 16/09/26 19/09/26
     "Dioggb3", -- rickzinrlx_20798 16/09/26 19/09/26
-    "maxnovo_009" -- menor_tralha777. 16/09/26 19/09/26
+    "maxnovo_009", -- menor_tralha777. 16/09/26 19/09/26
+    "black_01428" -- blessed004521 16/09/26 19/09/26
 }
