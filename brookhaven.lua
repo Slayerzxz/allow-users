a35e40e1bee25fe35f2d5948440ccfee54f3261c
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-17 01:14:28 -0300

    Admin panel: CassidyRoss42939

--- brookhaven.lua
+++ brookhaven.lua
@@ -132,5 +132,6 @@ return {
     "pedropis1231", -- pedroso04757 17/09/26 20/09/26
     "llovesthy", -- shydegalba_22969 17/09/26 20/09/26
     "Dudu157_TDR", -- dudu086746 17/09/26 20/09/26
-    "pdjffhgegrgrgb" -- mg_70411 17/09/26 20/09/26
+    "pdjffhgegrgrgb", -- mg_70411 17/09/26 20/09/26
+    "CassidyRoss42939" -- luan025164 17/09/26 20/09/26
 }
