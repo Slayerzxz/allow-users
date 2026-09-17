2fb95b55df9c4fb2c7904b3403b77db111325970
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:57:27 -0300

    Admin panel: Biu181985

--- brookhaven.lua
+++ brookhaven.lua
@@ -105,5 +105,6 @@ return {
     "Reidospiratas201311", -- kendrick0010886 16/09/26 19/09/26
     "Nex_titans", -- gb_black17 16/09/26 19/09/26
     "Carlinho1142", -- carlos031494 16/09/26 19/09/26
-    "wzssnts" -- mariyzzk7 16/09/26 19/09/26
+    "wzssnts", -- mariyzzk7 16/09/26 19/09/26
+    "Biu181985" -- thailan0457 16/09/26 19/09/26
 }
