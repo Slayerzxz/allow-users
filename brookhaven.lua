5404b615a7920054841f19c4f91bd7d0ca4e931d
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-17 00:46:13 -0300

    Admin panel: ZORO_HND1

--- brookhaven.lua
+++ brookhaven.lua
@@ -122,5 +122,6 @@ return {
     "L2_tui", -- irush01 17/09/26 20/09/26
     "Diulyan2030141D", -- draxmdx_51131 17/09/26 20/09/26
     "0vamp_zx0", -- dz7_157 17/09/26 20/09/26
-    "jhonnysiuza9" -- jhonny244_. 17/09/26 20/09/26
+    "jhonnysiuza9", -- jhonny244_. 17/09/26 20/09/26
+    "ZORO_HND1" -- alexamiri444 17/09/26 20/09/26
 }
