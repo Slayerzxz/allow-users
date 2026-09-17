70eb27aefa22892373a1e05a9aa0c2c07dd05a8a
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:24:02 -0300

    Admin panel: sharkzin2686

--- brookhaven.lua
+++ brookhaven.lua
@@ -78,5 +78,6 @@ return {
     "pedroleo50", -- eodryxxz_89777 16/09/26 19/09/26
     "xandy_057", -- xandy17_90418 16/09/26 19/09/26
     "REIDAVIGAMERLIN", -- davigamerlin3.9 16/09/26 19/09/26
-    "nascimento22" -- _nascimento22 16/09/26 19/09/26
+    "nascimento22", -- _nascimento22 16/09/26 19/09/26
+    "sharkzin2686" -- joaolucas0182_16889 16/09/26 19/09/26
 }
