7ab2e33adbe9407ceeb0f4a56ae10d5b32abbc94
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:49:16 -0300

    Admin panel: dirty01_kkj

--- brookhaven.lua
+++ brookhaven.lua
@@ -98,5 +98,6 @@ return {
     "mari123", -- claramirandarepolho 16/09/26 19/09/26
     "girldayana000", -- richrd0241_70577 16/09/26 19/09/26
     "bbzin7765", -- q01o3u 16/09/26 19/09/26
-    "lucasmacfrire" -- chamageladin 16/09/26 19/09/26
+    "lucasmacfrire", -- chamageladin 16/09/26 19/09/26
+    "dirty01_kkj" -- bryan7x0203_93968 16/09/26 19/09/26
 }
