76e9f9cb04cb4cbb5cd32b6d4bddc7084c8e288b
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:55:49 -0300

    Admin panel: Nex_titans

--- brookhaven.lua
+++ brookhaven.lua
@@ -102,5 +102,6 @@ return {
     "dirty01_kkj", -- bryan7x0203_93968 16/09/26 19/09/26
     "floxbox2021", -- tubers9307439 16/09/26 19/09/26
     "flesh2661", -- artur066874 16/09/26 19/09/26
-    "Reidospiratas201311" -- kendrick0010886 16/09/26 19/09/26
+    "Reidospiratas201311", -- kendrick0010886 16/09/26 19/09/26
+    "Nex_titans" -- gb_black17 16/09/26 19/09/26
 }
