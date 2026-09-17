b0667043110d637b1c6d2c5154b2b34abfe9d71b
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 22:23:44 -0300

    Admin panel: nascimento22

--- brookhaven.lua
+++ brookhaven.lua
@@ -77,5 +77,6 @@ return {
     "sadzin131", -- pietro 16/09/26 26/01/27
     "pedroleo50", -- eodryxxz_89777 16/09/26 19/09/26
     "xandy_057", -- xandy17_90418 16/09/26 19/09/26
-    "REIDAVIGAMERLIN" -- davigamerlin3.9 16/09/26 19/09/26
+    "REIDAVIGAMERLIN", -- davigamerlin3.9 16/09/26 19/09/26
+    "nascimento22" -- _nascimento22 16/09/26 19/09/26
 }
