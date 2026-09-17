854b954e6c144ce13d68fb1a572b92ec8c49c8a9
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-16 23:01:19 -0300

    Admin panel: kaio_9178

--- brookhaven.lua
+++ brookhaven.lua
@@ -113,5 +113,6 @@ return {
     "lorenzomo199", -- Lorenzo 16/09/26 01/02/54
     "Manuella44m95", -- manudivakj 16/09/26 19/09/26
     "WENDEKY7", -- gtarpbrasil 16/09/26 19/09/26
-    "davi_pa99h" -- daviflores0302 16/09/26 19/09/26
+    "davi_pa99h", -- daviflores0302 16/09/26 19/09/26
+    "kaio_9178" -- iarleipicanco 16/09/26 19/09/26
 }
