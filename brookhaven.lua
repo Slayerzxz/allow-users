ea8c995793d042967c0425f5a1a5e71689254960
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-18 01:30:41 -0300

    Remove joaoking667

--- brookhaven.lua
+++ brookhaven.lua
@@ -6,7 +6,6 @@ return {
     "vitinhofofinho54", -- Tugsahuuauau 27/08/26 27/12/26
     "itz_blackzinh01", -- Max 11/09/26 06/10/67
     "MV_ttk1234567", -- Yuki 29/08/26 25/05/29
-    "joaoking667", -- joaoexe66 29/08/26 29/09/26
     "darkizinha_6767", -- Slayer Hub | Moderation 30/08/26 29/09/26
     "Keel_m5", -- دونا 31/08/26 01/10/26
     "Matheus7374568", -- Filha da puta gamers 01/09/26 05/09/27
