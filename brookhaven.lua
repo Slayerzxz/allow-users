ffc528d94b5069d98a18b2d3415edb2f57efe592
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-20 22:00:53 -0300

    Remove fernandomt5

--- brookhaven.lua
+++ brookhaven.lua
@@ -42,7 +42,6 @@ return {
     "judelcy7", -- SolteirinhoBALA 18/09/26 14/06/29
     "Joan45858", -- Liviablox 16/09/26 31/01/54
     "Ravis_contasegund", -- joaoexe66 16/09/26 21/09/26
-    "fernandomt5", -- ꧁༒☬𝙂𝙊𝘿_𝙇𝙐𝙁𝙁𝙔☬༒꧂ 16/09/26 31/01/54
     "sadzin131", -- pietro 16/09/26 26/01/27
     "PHdaisa0", -- CABO MSC NASCIMENTO 🎸🎹🪊 16/09/26 26/09/26
     "lorenzomo199", -- Lorenzo 16/09/26 01/02/54
