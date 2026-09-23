cc5cdc1ce4f831adba8d2d4c770ecf61c6cde426
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-23 14:11:57 -0300

    Remove alnmk_7

--- brookhaven.lua
+++ brookhaven.lua
@@ -57,6 +57,5 @@ return {
     "mig108020", -- 72 22/09/26 01/11/26
     "misterioso3333333338", -- Slayer Hub | Admin 22/09/26 25/09/26
     "itz_kadu22", -- Bazuka հվ 23/09/26 24/09/26 testando admin
-    "eslly1892k8", -- Luiz 23/09/26 26/09/26
-    "alnmk_7" -- Samuelmorie 23/09/26 26/09/26
+    "eslly1892k8" -- Luiz 23/09/26 26/09/26
 }
