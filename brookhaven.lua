7622000d449cdc1d1f17c607b4130783a73a8706
Slayer <310001057+Slayerzxz@users.noreply.github.com>
2026-09-24 01:57:05 -0300

    Auto remove expirados: bielwqc

--- brookhaven.lua
+++ brookhaven.lua
@@ -53,7 +53,6 @@ return {
     "matheus_itz00", -- 𝖆𝖑𝖊𝖒ã𝖔🇩🇪 20/09/26 16/06/29
     "ketchup2228", -- Loritta 20/09/26 30/09/26
     "therealneverfall", -- haru 22/09/26 22/10/26
-    "bielwqc", -- Bieldamidiakkj 22/09/26 24/09/26
     "mig108020", -- 72 22/09/26 01/11/26
     "misterioso3333333338", -- Slayer Hub | Admin 22/09/26 25/09/26
     "itz_kadu22", -- Bazuka հվ 23/09/26 24/09/26 testando admin
